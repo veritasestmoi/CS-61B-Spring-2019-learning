139638b14871a75e54aa1de5064535c37ab49580
Veritas <patkuo15@gmail.com>
2021-10-11 16:15:01 +0800

    hw

--- hw1/OurFirstJavaProgram.java
+++ hw1/OurFirstJavaProgram.java
@@ -1,15 +1,15 @@
 /**********************Our First Java Program********************/
 
-int size = 27;
-String name = "Fido";
-Dog myDog = new Dog(name, size);
-int x = size - 5;
-if (x < 15) {
+int size = 27; //Declare a varaible of type int and assigns it 
+String name = "Fido"; 
+Dog myDog = new Dog(name, size); //Declares and initializes a new variable of type Dog. Calls the dog constructor to create a new object of type Dog
+int x = size - 5; //Declares a nnw var of type int and assigns it the val 22 
+if (x < 15) { //no
 myDog.bark(8);
 }
 
-while (x > 3) {
-x -= 1;
+while (x > 3) { //yes
+x -= 1; //24
 myDog.play();
 }
 
@@ -21,3 +21,5 @@ System.out.println(numList[1]);
 if (numList[3] == 8) {
 System.out.println("potato");
 }
+
+//https://yintelligence.tech/2021/02/28/2021-02-28-Introduction-to-Java/
